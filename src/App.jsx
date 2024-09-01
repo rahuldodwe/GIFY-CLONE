9fafff34a9c7521e9ae611e1687c4088699db180
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-09-01 21:16:45 +0530

    gif api connection is remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layouts/app-layout";
+import Favorites from "./pages/favorites";
+import Home from "./pages/home";
+import Category from "./pages/category";
+import SearchPage from "./pages/search";
+import GifPage from "./pages/single-gif";
+import GifProvider from "./contex/gif-context";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:category",
+        element: <Category />,
+      },
+      {
+        path: "/search/:qury",
+        element: <SearchPage />,
+      },
+      {
+        path: "/:type/:slug",
+        element: <GifPage />,
+      },
+      {
+        path: "/favorites",
+        element: <Favorites />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <GifProvider>
+      <RouterProvider router={router} />;
+    </GifProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/contex/gif-context.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+
+const GifContext = createContext();
+
+const GifProvider = ({children}) => {
+    return <GifContext.Provider>{children}</GifContext.Provider>
+}
+
+export default GifProvider
